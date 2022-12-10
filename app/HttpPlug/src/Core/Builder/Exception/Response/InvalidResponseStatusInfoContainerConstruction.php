f008a9057123d30d16ba70e214486de60792c902
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 23:20:25 +0100

    added InvalidResponseStatusInfoContainerConstruction

---
+++ app/HttpPlug/src/Core/Builder/Exception/Response/InvalidResponseStatusInfoContainerConstruction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Response;
+
+use Merce\RestClient\HttpPlug\src\Exception\IException;
+
+class InvalidResponseStatusInfoContainerConstruction extends \Exception implements IException {
+
+}
