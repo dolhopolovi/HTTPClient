8faf9d9c7e9acd4ebd930142568583ffd9a10cd1
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 23:20:12 +0100

    added InvalidResponseConstruction

---
+++ app/HttpPlug/src/Core/Builder/Exception/Response/InvalidResponseConstruction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Response;
+
+use Merce\RestClient\HttpPlug\src\Exception\IException;
+
+class InvalidResponseConstruction extends \Exception implements IException {
+
+}
