808194d610aae54e59f303655c461af734d79d4f
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 23:25:24 +0100

    added NotImplementedException

---
+++ app/HttpPlug/src/Exception/Impl/NotImplementedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Exception\Impl;
+
+use Merce\RestClient\HttpPlug\src\Exception\IException;
+
+class NotImplementedException extends \BadMethodCallException implements IException
+{
+
+}
