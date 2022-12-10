0894d522c9ba236c17488f87a603b66dad1aa6f3
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 22:55:12 +0100

    added AuthMiddleware

---
+++ app/HttpPlug/src/Core/Middleware/Impl/AuthMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Merce\RestClient\HttpPlug\src\Core\Middleware\Impl;
+
+use Http\Message\Authentication;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Client\ClientExceptionInterface;
+use Merce\RestClient\HttpPlug\src\Core\Middleware\IMiddleware;
+
+
+class AuthMiddleware implements IMiddleware
+{
+
+    private function __construct(
+        private readonly Authentication $authentication
+    ) {
+    }
+
+    /**
+     * @param  RequestInterface  $request
+     * @return RequestInterface
+     * @throws \Psr\SimpleCache\InvalidArgumentException|ClientExceptionInterface
+     */
+    public function handleForRequest(RequestInterface $request): RequestInterface
+    {
+
+        return $request->withAddedHeader('Authorization', $this->authentication->authenticate($request));
+
+    }
+
+    /**
+     * @param  RequestInterface  $request
+     * @param  ResponseInterface  $response
+     * @return ResponseInterface
+     */
+    public function handleForResponse(RequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        return $response;
+    }
+}
