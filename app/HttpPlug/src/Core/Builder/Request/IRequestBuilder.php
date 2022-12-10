62cb3a2fb11072c2a600d28dab852fb912a011cd
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 23:20:36 +0100

    added IRequestBuilder

---
+++ app/HttpPlug/src/Core/Builder/Request/IRequestBuilder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Core\Builder\Request;
+
+use Psr\Http\Message\RequestInterface;
+use Merce\RestClient\HttpPlug\src\Support\EHttpMethod;
+
+interface IRequestBuilder {
+
+    public function setMethod(EHttpMethod $method): self;
+    public function setUri(string $uri): self;
+    public function setHeaders(array $headers): self;
+    public function setBody(string $body): self;
+    public function getRequest(): RequestInterface;
+}
