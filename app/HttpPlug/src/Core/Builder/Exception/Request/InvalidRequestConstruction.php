4dc5a4764dda1481229b1c72a4bcfbc3e1589ca9
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-10 23:19:56 +0100

    added InvalidRequestConstruction

---
+++ app/HttpPlug/src/Core/Builder/Exception/Request/InvalidRequestConstruction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Request;
+
+use Merce\RestClient\HttpPlug\src\Exception\IException;
+
+class InvalidRequestConstruction extends \Exception implements IException {
+
+}
