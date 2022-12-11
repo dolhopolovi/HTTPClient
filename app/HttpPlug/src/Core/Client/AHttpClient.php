923a064fa249f7fec5e623e082e194df22b3f3b5
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 20:15:20 +0100

    minor chnages

--- app/HttpPlug/src/Core/Client/AHttpClient.php
+++ app/HttpPlug/src/Core/Client/AHttpClient.php
@@ -4,13 +4,13 @@ declare(strict_types = 1);
 
 namespace Merce\RestClient\HttpPlug\src\Core\Client;
 
-use Merce\RestClient\HttpPlug\src\Service\Client\Curl\Core\IClientService;
+use Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\Factory\IFactoryCurlBuilder;
 
 abstract class AHttpClient
 {
 
     public function __construct(
-        protected IClientService $clientService,
+        protected IFactoryCurlBuilder $curlHandler,
     ) {
     }
 }
