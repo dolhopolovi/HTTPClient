4a324fa0ed0ba288ced994a30a0e69efb5eaabc8
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 22:08:07 +0100

    minor fix

--- app/HttpPlug/src/DTO/Curl/Request/IGenericCurlExtraParamPack.php
+++ app/HttpPlug/src/DTO/Curl/Request/IGenericCurlExtraParamPack.php
@@ -4,7 +4,7 @@ namespace Merce\RestClient\HttpPlug\src\DTO\Curl\Request;
 
 interface IGenericCurlExtraParamPack
 {
-    public function setCURLOPTVERBOSE($CURLOPT_VERBOSE): self;
+    public function setCURLOPTVERBOSE(bool $CURLOPT_VERBOSE): self;
     public function setCURLOPTFAILONERROR(bool $CURLOPT_FAILONERROR): self;
     public function setCURLOPTRETURNTRANSFER(bool $CURLOPT_RETURNTRANSFER): self;
     public function setCURLOPTHEADER(bool $CURLOPT_HEADER): self;
