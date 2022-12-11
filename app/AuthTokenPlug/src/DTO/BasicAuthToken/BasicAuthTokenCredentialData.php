c0788b92fa3d2292dae9b0b793d118a4fe08afe2
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-12 00:01:01 +0100

    code style chnages

--- app/AuthTokenPlug/src/DTO/BasicAuthToken/BasicAuthTokenCredentialData.php
+++ app/AuthTokenPlug/src/DTO/BasicAuthToken/BasicAuthTokenCredentialData.php
@@ -1,15 +1,19 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Merce\RestClient\AuthTokenPlug\src\DTO\BasicAuthToken;
 
 use Merce\RestClient\HttpPlug\src\Exception\Impl\InvalidArgumentException;
 
 class BasicAuthTokenCredentialData
 {
+
     public function __construct(
         public readonly string $username,
         public readonly string $password
     ) {
+
         if (empty($this->username) || empty($this->password)) {
             throw new InvalidArgumentException('Error: empty credentials');
         }
@@ -17,6 +21,7 @@ class BasicAuthTokenCredentialData
 
     public function __toString(): string
     {
+
         return base64_encode($this->username . ':' . $this->password);
     }
 }
