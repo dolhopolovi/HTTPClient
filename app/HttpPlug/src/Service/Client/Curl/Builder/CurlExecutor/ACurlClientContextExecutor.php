41c6cf90573dd1906b53e8c62e134cb448ea45fe
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 20:14:53 +0100

    added ACurlClientContextExecutor

---
+++ app/HttpPlug/src/Service/Client/Curl/Builder/CurlExecutor/ACurlClientContextExecutor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\CurlExecutor;
+
+use Psr\Http\Message\ResponseInterface;
+use Merce\RestClient\HttpPlug\src\DTO\Curl\Request\ICurlRequestPack;
+
+abstract class ACurlClientContextExecutor
+{
+
+    public function __construct(
+        private ICurlRequestPack $curlGenericRequestBuilder
+    ) {
+    }
+
+    public abstract function execute(): ResponseInterface;
+    protected abstract function parseError(): void;
+
+    protected abstract function __destruct();
+}
