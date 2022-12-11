ee983561fb981ae025de1865690aec85e37cef73
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 20:19:14 +0100

    added FactoryCurlBuilderFactory

---
+++ app/HttpPlug/src/Service/Client/Curl/Builder/Factory/Impl/FactoryCurlBuilderFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\Factory\Impl;
+
+use Merce\RestClient\HttpPlug\src\DTO\Curl\Request\IGenericCurlRequestDTO;
+use Merce\RestClient\HttpPlug\src\DTO\Curl\Request\Impl\GenericCurlRequestDTO;
+use Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\CurlBuilder\ICurlBuilder;
+use Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\Factory\IFactoryCurlBuilder;
+use Merce\RestClient\HttpPlug\src\Service\Client\Curl\Builder\CurlBuilder\Impl\CurlBuilder;
+
+class FactoryCurlBuilderFactory implements IFactoryCurlBuilder {
+
+    public function __construct() {}
+
+    public function init(IGenericCurlRequestDTO $genericCurlRequestDTO = new GenericCurlRequestDTO()): ICurlBuilder {
+        return new CurlBuilder($genericCurlRequestDTO);
+    }
+}
