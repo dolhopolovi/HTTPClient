fffe953d531df119dff1125c17ad687188230c2e
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 23:40:46 +0100

    added RequestServiceTest

---
+++ tests/Unit/HttpPlug/Service/Builder/Request/RequestServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Merce\RestClient\Test\Unit\HttpPlug\Service\Builder\Request;
+
+use PHPUnit\Framework\TestCase;
+use Merce\RestClient\HttpPlug\src\Service\Builder\Request\IRequestService;
+use Merce\RestClient\HttpPlug\src\Service\Builder\Request\Impl\RequestService;
+use Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Request\InvalidRequestConstruction;
+
+class RequestServiceTest extends TestCase
+{
+    private IRequestService $requestService;
+
+    public function testGetHttpMethod() {
+
+        $this->expectException(InvalidRequestConstruction::class);
+        $this->expectErrorMessage('Error: request http method null exception');
+
+        $this->requestService->getHttpMethod();
+    }
+
+    public function testGetUri() {
+
+        if($this->requestService->getHeaderCollection()->isEmpty()) {
+            $this->assertTrue(true);
+        } else
+            $this->fail();
+    }
+
+    public function testGetHeaderCollection() {
+
+        $this->expectException(InvalidRequestConstruction::class);
+        $this->expectErrorMessage('Error: request url null exception');
+
+        $this->requestService->getUri();
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->requestService = new RequestService();
+    }
+}
