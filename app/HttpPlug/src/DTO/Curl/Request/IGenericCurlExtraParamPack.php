3f151d085440e6e6796f8668f693213cd78b17f1
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-11 20:21:34 +0100

    added IGenericCurlExtraParamPack

---
+++ app/HttpPlug/src/DTO/Curl/Request/IGenericCurlExtraParamPack.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Merce\RestClient\HttpPlug\src\DTO\Curl\Request;
+
+interface IGenericCurlExtraParamPack
+{
+    public function setCURLOPTVERBOSE($CURLOPT_VERBOSE): self;
+    public function setCURLOPTFAILONERROR(bool $CURLOPT_FAILONERROR): self;
+    public function setCURLOPTRETURNTRANSFER(bool $CURLOPT_RETURNTRANSFER): self;
+    public function setCURLOPTHEADER(bool $CURLOPT_HEADER): self;
+    public function get(): array;
+}
