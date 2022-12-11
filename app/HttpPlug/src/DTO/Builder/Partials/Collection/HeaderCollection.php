d830009fa4eb0af901dc6b6d4a948313c2b5d7be
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-12 00:02:49 +0100

    code style chnages

--- app/HttpPlug/src/DTO/Builder/Partials/Collection/HeaderCollection.php
+++ app/HttpPlug/src/DTO/Builder/Partials/Collection/HeaderCollection.php
@@ -1,49 +1,62 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Merce\RestClient\HttpPlug\src\DTO\Builder\Partials\Collection;
 
+use Iterator;
 use Merce\RestClient\HttpPlug\src\DTO\Builder\Partials\HeaderDTO;
 
-class HeaderCollection implements \Iterator
+class HeaderCollection implements Iterator
 {
 
-    /* @var HeaderDTO[] $collection*/
+    /* @var HeaderDTO[] $collection */
     private array $collection;
 
-    public function __construct(HeaderDTO ...$collection) {
+    public function __construct(HeaderDTO ...$collection)
+    {
+
         $this->collection = $collection;
     }
 
-    public function append(HeaderDTO $headerDTO): void {
+    public function append(HeaderDTO $headerDTO): void
+    {
+
         $this->collection[] = $headerDTO;
     }
 
-    public function current(): HeaderDTO {
+    public function current(): HeaderDTO
+    {
 
         return current($this->collection);
     }
 
     public function next(): void
     {
+
         next($this->collection);
     }
 
     public function key(): int
     {
+
         return key($this->collection);
     }
 
     public function valid(): bool
     {
+
         return current($this->collection) !== false;
     }
 
     public function rewind(): void
     {
+
         reset($this->collection);
     }
 
-    public function isEmpty(): bool {
+    public function isEmpty(): bool
+    {
 
         return empty($this->collection);
     }
