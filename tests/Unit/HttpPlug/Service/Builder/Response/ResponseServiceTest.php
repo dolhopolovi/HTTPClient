d5bbdfcd550625dfcdaf1e4628d53792ab0cccef
Ihor-Severyn Dolhopolov <ihor-severyn_dolhopolov@polcom.com.pl>
2022-12-12 05:38:01 +0100

    minor changes

--- tests/Unit/HttpPlug/Service/Builder/Response/ResponseServiceTest.php
+++ tests/Unit/HttpPlug/Service/Builder/Response/ResponseServiceTest.php
@@ -4,10 +4,17 @@ namespace Merce\RestClient\Test\Unit\HttpPlug\Service\Builder\Response;
 
 use ReflectionClass;
 use PHPUnit\Framework\TestCase;
+use Merce\RestClient\HttpPlug\src\DTO\Builder\Response\ResponseDTO;
+use Merce\RestClient\HttpPlug\src\Service\Builder\Response\IResponseService;
+use Merce\RestClient\HttpPlug\src\DTO\Builder\Response\ResponseStatusInfoDTO;
 use Merce\RestClient\HttpPlug\src\Service\Builder\Response\Impl\ResponseService;
+use Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Response\InvalidResponseConstruction;
+use Merce\RestClient\HttpPlug\src\Core\Builder\Exception\Response\InvalidResponseStatusInfoContainerConstruction;
 
 class ResponseServiceTest extends TestCase
 {
+    private IResponseService $responseService;
+
     public function testAppendHeader() {
         $response = new ResponseService();
         $reflection = new ReflectionClass($response);
